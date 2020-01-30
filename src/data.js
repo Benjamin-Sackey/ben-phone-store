7572ec56163653ec8c24ea01291d8d0821232510
Benjamin Sackey <benjaminsackey07@gmail.com>
2020-01-30 11:07:46 +0000

    first commit

---
+++ src/data.js
@@ -0,0 +1,112 @@
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Apple iPhone 11 Pro",
+    img: "img/product-1.png",
+    price: 999,
+    company: "APPLE",
+    info:
+      "The iPhone 11 Pro and iPhone 11 Pro Max are smartphones designed, developed and marketed by Apple Inc. They are the 13th-generation flagships of the iPhone, succeeding the iPhone XS and iPhone XS Max, respectively. Apple CEO Tim Cook unveiled the devices alongside a lower-end model, the iPhone 11, on September 10, 2019, at the Steve Jobs Theater at Apple Park. Pre-orders began on September 13, 2019, and went on sale on September 20.[6]Key upgrades over the previous devices include the triple-lens rear camera system and the A13 Bionic chip.[7] The 11 Pro is Apple's first iPhone to feature a pro designation, which was previously used only for larger Apple devices, such as the iPad Pro and MacBook Pro,[8] and the first to include in the box an 18W fast charger and a Lightning to USB-C cable that allows connection to current Mac computers.[9]",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung Galaxy S10",
+    img: "img/product-2.png",
+    price: 799,
+    company: "SAMSUNG",
+    info:
+      "Samsung Galaxy S10 is a line of Android-based smartphones manufactured, released and marketed by Samsung Electronics. The Galaxy S10 series is a celebratory series of the 10th anniversary of the Samsung Galaxy S flagship line, its top line of phones next to the Note models. Unveiled during the Samsung Galaxy Unpacked 2019 press event held on February 20, 2019, the devices started shipping in certain regions such as Australia and the United States on March 6, 2019, then worldwide on March 8, 2019.[5] It is the tenth generation of Samsung's Galaxy S series of smartphones.As has been done since the Galaxy S6, Samsung unveiled flagship Galaxy S10 and Galaxy S10+ models, differentiated primarily by screen size and an additional front-facing camera on the S10+. In addition, Samsung also unveiled a smaller model known as the Galaxy S10e, as well as a larger, 5G-compatible version, the Galaxy S10 5G.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Google Pixel 4 XL",
+    img: "img/product-3.png",
+    price: 789,
+    company: "Google",
+    info:
+      "Pixel 4 and Pixel 4 XL are Android smartphones from the Google Pixel product line, serving as successors to the Pixel 3.[4][5] The phones were officially announced on October 15, 2019 and released in the United States on October 24, 2019.[6] In the United States, the Pixel 4 is the first Pixel phone to be offered for sale by all major wireless carriers at launch. Previous flagship Pixel models had launched as exclusives to Verizon and Google Fi; the midrange Pixel 3a was additionally available from Sprint and T-Mobile, but not AT&T, at its launch.[9] As with all other Pixel releases, Google is offering unlocked U.S. versions through its website.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Huawei Y9 Prime",
+    img: "img/product-4.png",
+    price: 219,
+    company: "Huawei",
+    info:
+      "The HUAWEI Y9 Prime 2019 features a unique dividing line that makes this design standout from the rest. On the smooth back, the special silk texture shimmers under the light and you can see the fine details clearly The extraordinary 6.59 HUAWEI Ultra FullView Display1 delivers an ultra-wide horizon with zero notch. Experience limitless fun while watching videos, playing games or reading e-books, all on an almost bezel-less screen. The 16 MP AI front camera offers real-time image optimising in 8 scenes and will intelligently adjust the retouching intensity. Capture your stunning selfies with HUAWEI Y9 Prime 2019 from any angle.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Tecno Camon 12",
+    img: "img/product-5.png",
+    price: 132,
+    company: "Tecno",
+    info:
+      "Camon 12 comes back with 16+2+5MP AI Max Triple Rear Camera and its four outstanding shooting performances. Ultra Clear Shot / 120 Super Wide Angle / 2cm Extreme Macro Photography / Remarkable Bokeh Effect, offer you chance to seize wonderful moments from different angles. Step foward to unlock your brand new era, to discover more beauty of the world. Unlock A New CAM*ERA with CAMON 12, you would see the world much clearer, closer, brighter and wider.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Infinix S5",
+    img: "img/product-6.png",
+    price: 189,
+    company: "Infinix",
+    info:
+      "Meet the 6.6 inch 20:9 aspect ratio cinematic wide Infinity-O display. With aslimmer bezel-less design at 90.5%screen-to-body ratio, immerse yourself inbrilliant experiences and enjoy the vivid details. The 32MP In-Display AI Camera with priemum AI technology, capturesvivid details and true-to-life colors, especially in the low-light and backlightscenes. It provides 6528 x 4896 super high resolution to bring you out thesharp, clear and vivid detailed selfie images.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "OnePlus 7 Pro",
+    img: "img/product-7.png",
+    price: 499,
+    company: "OnePlus",
+    info:
+      "The 6.67 inch Fluid AMOLED display on the OnePlus 7 Pro is our most advanced screen ever. Experience unrivalled smoothness and clarity with a 90 Hz refresh rate and QHD+ resolution. Experience a high-resolution display with a 90 Hz refresh rate, designed to display your content with dramatic clarity. The display curves over the edges for an arresting design and an immersive viewing experience. Upgraded Screen Unlock technology ensures you can reliably unlock your OnePlus 7 Pro in just 0.21 seconds. Every tap, swipe and button press feels instant and effortless thanks to the world’s first AMOLED display with a 90 Hz refresh rate.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Sony Xperia 1",
+    img: "img/product-8.png",
+    price: 949,
+    company: "Sony",
+    info:
+      "The Sony Xperia 1 is an Android smartphone manufactured and marketed by Sony.[3] Part of Sony's flagship Xperia series, the device was announced to the public at a press conference held at the annual 2019 Mobile World Congress event on February 25, 2019.[4] It is the world's first smartphone with an ultrawide 21:9 aspect ratio 4K HDR OLED display, dubbed CinemaWide, and Sony's first triple lens camera system featuring Eye AF eye-tracking technology seen in the company's Alpha line of professional-grade cameras.[5] The Xperia 1 was later joined by a more compact variant, the Xperia 5, which was revealed on 5 September 2019. Compared to the Xperia 1, the Xperia 5 has a smaller 1080p screen, a smaller battery and a non-centrally aligned camera module.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+    title: "Apple iPhone 11 Pro",
+    img: "img/product-1.png",
+    price: 999,
+    company: "APPLE",
+    info:
+      "The iPhone 11 Pro and iPhone 11 Pro Max are smartphones designed, developed and marketed by Apple Inc. They are the 13th-generation flagships of the iPhone, succeeding the iPhone XS and iPhone XS Max, respectively. Apple CEO Tim Cook unveiled the devices alongside a lower-end model, the iPhone 11, on September 10, 2019, at the Steve Jobs Theater at Apple Park. Pre-orders began on September 13, 2019, and went on sale on September 20.[6]Key upgrades over the previous devices include the triple-lens rear camera system and the A13 Bionic chip.[7] The 11 Pro is Apple's first iPhone to feature a pro designation, which was previously used only for larger Apple devices, such as the iPad Pro and MacBook Pro,[8] and the first to include in the box an 18W fast charger and a Lightning to USB-C cable that allows connection to current Mac computers.[9]",
+    inCart: false,
+    count: 0,
+    total: 0
+};
